cffeda5d3aeb461655f8bd595f4149d85ffee6fd
mulac <wallbridge.calum@gmail.com>
2021-05-14 18:27:48 +0100

    empty files

--- main.go
+++ main.go
@@ -10,6 +10,10 @@ import (
 func main() {
 	fmt.Println("hello")
 
+	for id := range a list of Ids{
+		scraper.Youtube().Downlaod(id)
+	}
+
 	f, err := os.Open("README.md")
 	if err != nil {
 		panic(err)
